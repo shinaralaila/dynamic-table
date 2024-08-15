b208f9749f5b62d097b57af0c616a4f7a7de24a1
Shinara <86114176+shinaralaila@users.noreply.github.com>
2024-08-15 16:33:40 +0800

    second commit

--- FrontEnd/src/Components/Form.component.js
+++ FrontEnd/src/Components/Form.component.js
@@ -114,7 +114,7 @@ const Form = () => {
               <button
                 className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                 type="submit"
-                // disabled={!row || !column}
+                
               >
                 Submit
               </button>
